eaab382f0b9e91b0ad8d4fdea89dc8c5aefbf61d
Lorenzo Miñón <lorenzominon01@gmail.com>
2023-12-06 18:18:08 -0300

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import { Container } from 'react-bootstrap';
 import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { About } from './pages/About';
-
+import { NavBar } from './components/NavBar';
 
 function App() {
 
   return (
-    <Container className='mb-4'>
-      <Routes>
-        <Route path= "/" element={<Home />}/>
-        <Route path= "/store" element={<Store />}/>
-        <Route path= "/about" element={<About />}/>
-      </Routes>
-    </Container>
+    <>
+      <NavBar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path= "/" element={<Home />}/>
+          <Route path= "/store" element={<Store />}/>
+          <Route path= "/about" element={<About />}/>
+        </Routes>
+       </Container>
+
+    </>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import { Button, Nav, Container, Navbar as NavBarBs } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom'
+
+export function NavBar(){
+    return(
+        <NavBarBs className= "bg-white shadow-sm mb-3">
+            <Container>
+                <Nav className="me-auto"> {/*Pone margin del lado derecho automatico*/}
+                    <Nav.Link to="/" as={NavLink}> 
+                        Home
+                    </Nav.Link>
+                    <Nav.Link to="/store" as={NavLink}>
+                        Store
+                    </Nav.Link>
+                    <Nav.Link to="/about" as={NavLink}>
+                        About
+                    </Nav.Link>
+                </Nav>
+                <Button style={{width: "3rem", height:"3rem", position: 'relative'}}
+                variant="outline-primary"
+                className="rounded-circle"> 
+                {/*rounded circle para futuro ver cantidad de items que tenemos!*/}
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/>
+                    </svg>
+                    <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{color:"white"}}>
+                        3
+                    </div>
+                </Button>
+            </Container>
+        </NavBarBs>
+    )
+}
