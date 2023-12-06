0cdd2c9eb624c3fad35ab2f2a5ca9f9a3a533a21
Lorenzo Miñón <lorenzominon01@gmail.com>
2023-12-06 17:48:48 -0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
+
 
 function App() {
 
   return (
-    <h1>Hola</h1>
+    <Container className='mb-4'>
+      <Routes>
+        <Route path= "/" element={<Home />}/>
+        <Route path= "/store" element={<Store />}/>
+        <Route path= "/about" element={<About />}/>
+      </Routes>
+    </Container>
   )
 }
 
